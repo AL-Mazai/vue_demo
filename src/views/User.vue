7af921f4337d8524f208ab7dd6739f70476272ba
zzw <3279527604@qq.com>
2023-04-15 11:38:19 +0800

    vue示例

---
+++ src/views/User.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <table>
+            <tr>
+                <td>编号</td>
+                <td>名字</td>
+            </tr>
+            <tr v-for="book in users">
+                <td>{{book.id}}</td>
+                <td>{{book.name}}</td>
+            </tr>
+        </table>
+    </div>
+</template>
+
+<script>
+import axios from "core-js/internals/queue";
+
+export default {
+    name: "User",
+    data(){
+        return {
+            users:[
+                {
+                    id: 1,
+                    name: '三体',
+                },
+                {
+                    id: 2,
+                    name: 'java从零到入土',
+                }
+            ]
+        }
+    },
+    created() {
+        // let that = this;
+        axios.get('http://localhost:8084/get').then(function (resp) {
+            this.users = resp.data;
+        })
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
