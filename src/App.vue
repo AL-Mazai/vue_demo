e1b9edd90460a3540ea73cb65341cc537f2b3f71
zzw <3279527604@qq.com>
2023-04-21 16:32:54 +0800

    修改了从后端获取User信息的方法

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,9 @@
     <nav>
         <router-link to="/">Home</router-link>
         |
-        <router-link to="/about">About</router-link>
-        |
         <router-link to="/user">User</router-link>
     </nav>
-    <router-view/>
+    <router-view></router-view>
 </template>
 
 <style>
@@ -16,6 +14,7 @@
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
+    /*background-color: red;*/
 }
 
 nav {
--- src/views/User.vue
+++ src/views/User.vue
@@ -5,42 +5,44 @@
                 <td>编号</td>
                 <td>名字</td>
             </tr>
-            <tr v-for="book in users">
-                <td>{{book.id}}</td>
-                <td>{{book.name}}</td>
+            <tr v-for="user in users"  :key="user.userId">
+                <td>{{user.userId}}</td>
+                <td>{{user.userName}}</td>
             </tr>
         </table>
     </div>
 </template>
 
 <script>
-import axios from "core-js/internals/queue";
+import axios from 'axios'
 
 export default {
+    // eslint-disable-next-line vue/multi-word-component-names
     name: "User",
     data(){
         return {
-            users:[
-                {
-                    id: 1,
-                    name: '三体',
-                },
-                {
-                    id: 2,
-                    name: 'java从零到入土',
-                }
-            ]
+            users:[]
         }
     },
     created() {
-        // let that = this;
-        axios.get('http://localhost:8084/get').then(function (resp) {
-            this.users = resp.data;
-        })
+        this.getUser()
+    },
+    methods:{
+        getUser(){
+            const vm = this
+            axios.get('http://localhost:8084/get').then(function(resp) {
+                vm.users = resp.data;
+            })
+        }
     }
 }
 </script>
 
 <style scoped>
-
+table {
+    font-size:20px;
+    border:1px solid black;
+    margin-left:auto;
+    margin-right:auto
+}
 </style>
